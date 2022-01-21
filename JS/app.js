5808807e80a2d89a43375ba6b05fed50de0d6396
SOK.NHOK <sok.nhok@PNC.LAN>
2022-01-21 20:19:43 +0700

    creater header

--- JS/app.js
+++ JS/app.js
@@ -4,12 +4,14 @@
 
 // Forward new page--------------------------------------------
 function nextPage(){
-    const inputName = document.getElementById('user-name');
+    var inputName = document.getElementById('user-name');
     if (inputName.value === ""){
         return confirm("You must input your name!");
     } else{
         displayNextWebpage(event);
+        user_name = inputName.value;
     }
+    console.log(inputName.value);
 }
 
 // Remove old page and create new menu--------------------------
@@ -81,6 +83,48 @@ btnNext.addEventListener("click",nextPage);
 function inProgress(event){
     let container = document.querySelector('.container');
     container.style.display = "none";
+    // create header 
+    let header = document.createElement("header");
+    header.className = "header";
+    //create header left and append it to header
+    let header_left = document.createElement("div");
+    header_left.className = "header-left";
+    // creater div and set class name to logo 
+    let logo = document.createElement("div");
+    logo.className = "logo";
+    // create img tage set class name as img
+    let img = document.createElement("img")
+    img.className = "img";
+    img.src = "images/pnc-logo.png"
+    img.style.width = '80px'
+    // create header left title
+    let logo_title = document.createElement("h1");
+    logo_title.className = "logo-title";
+    logo_title.textContent = "StudyEnglishHere";
+    // create header right and append it to header 
+    let header_right = document.createElement("div");
+    header_right.className = "header-right";
+    
+    // get username input
+    // let inputName = document.getElementById('user-name');
+    let userName = document.createElement('h2');
+    userName.textContent =  user_name;
+    
+    header_right.appendChild(userName);
+
+    // append img to logo div and append logo to header left div
+    logo.appendChild(img)
+    header_left.appendChild(logo)
+    header_left.appendChild(logo_title)
+    header.appendChild(header_left)
+    console.log(header)
+    document.body.appendChild(header)
+
+    header.appendChild(header_right);
+    
 
 }
 // end header-----------------------------------------------------
+
+
+let user_name = "";
